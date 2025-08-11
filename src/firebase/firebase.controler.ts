e2a016fb742043d33891a0a89affb5eb3ec8e0eb
Ricardo Matta <ricardommps@gmail.com>
2025-08-11 08:45:26 -0300

    Ajuste

--- src/firebase/firebase.controler.ts
+++ src/firebase/firebase.controler.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { NotificationType } from 'src/types/notification.type';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../utils/user-type.enum';
 import { FirebaseService } from './firebase.service';
@@ -8,18 +7,9 @@ import { FirebaseService } from './firebase.service';
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
 
-  @Roles(UserType.Admin, UserType.Root, UserType.User)
-  @Get('sendNotification/:customerId')
-  async sendNotification(@Param('customerId') customerId) {
-    return await this.firebaseService.sendNotification(customerId);
-  }
-
   @Roles(UserType.Admin, UserType.Root)
   @Post('sendNotificationNew/:customerId')
-  async loginCustomer(
-    @Param('customerId') customerId,
-    @Body() messages: NotificationType,
-  ) {
+  async loginCustomer(@Param('customerId') customerId, @Body() messages: any) {
     return this.firebaseService.sendNotificationNew(customerId, messages);
   }
 }
