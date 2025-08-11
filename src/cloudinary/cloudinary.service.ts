7eaaf4d0c381d2de5bed64bdd725aae0bd87a768
Ricardo Matta <ricardommps@gmail.com>
2025-08-11 07:12:21 -0300

    Correção no upload

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as toStream from 'buffer-to-stream';
+import toStream from 'buffer-to-stream';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import { File as MulterFile } from 'multer'; // 👈 importando o tipo
 
